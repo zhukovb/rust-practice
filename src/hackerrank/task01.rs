e591ddfa347337faf00986c873a46f0bebe3f113
zhukovb <bohdan.zhukov@e-u.edu.ua>
2025-03-20 00:02:07 +0200

    Розв’язав задачу з HackerRank

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,22 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
-}
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+n = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+
+print(simpleArraySum(ar))
